742683d3896402de9c0451a0ad01ec162c90fa4c
Luiz Gustavo de Almeida Cruz - Kanguru Conexões Inteligentes <luiz.cruz@kan.guru>
2022-05-13 15:09:43 -0300

    3

---
+++ src/pages/detail/index.js
@@ -0,0 +1,10 @@
+//Esta parte do codigo representa a tela de Detalhes da minha aplicação//
+
+import React from 'react';
+import { View ,Text } from 'react-native';
+
+export default function detail() {
+ return (
+   <view>  </view>
+  );
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+//Esta parte do codigo representa a tela home da minha aplicação//
+
+import React from 'react';
+import { View ,Text } from 'react-native';
+
+export default function home() {
+ return (
+   <view>  </view>
+  );
+}
